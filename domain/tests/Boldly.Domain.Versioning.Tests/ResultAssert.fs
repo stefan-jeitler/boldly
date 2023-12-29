bab86013621ac5c8e8545470420b3826790cdff7
Stefan <stefan.jeitler@outlook.com>
2023-12-29 22:56:17 +0100

    chore: re-structure domain projects

---
+++ domain/tests/Boldly.Domain.Versioning.Tests/ResultAssert.fs
@@ -0,0 +1,6 @@
+﻿module Boldly.Domain.Tests.ResultAssert
+
+open Xunit
+
+let okNotExpected<'a>(v: 'a) = Assert.Fail($"Expected an Error, but got Ok '{v}'")
+let errorNotExpected<'a>(e: 'a) = Assert.Fail($"Expected Ok, but got an error '{e}'")
